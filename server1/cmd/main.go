df5d86fe60bd23795a4a02225a7a0d9e909a55b0
liangxiangtao <liangliang.too@foxmail.com>
2023-02-04 15:23:22 +0800

    my project base struct

---
+++ server1/cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	routeV1 "github.com/LucienVen/go-web-demo/server1/api/route/v1"
+	"github.com/LucienVen/go-web-demo/server1/bootstrap"
+	"github.com/LucienVen/go-web-demo/server1/internal/log"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello, server1")
+
+	app := bootstrap.App()
+
+	env := app.Env
+	db := app.Mysql
+	defer app.CloseApplication()
+
+	// 初始化日志组件
+	log.InitLogger(env)
+
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+
+	r := gin.Default()
+
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "hello, server1",
+	//	})
+	//})
+
+	//routeV1.Handle(r)
+
+	routerV1 := r.Group("v1")
+
+	routeV1.Setup(env, timeout, db, routerV1)
+
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
