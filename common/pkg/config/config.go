06f2b3a31ead8468b55cb2deb186ddbd0cadc586
liangxiangtao <liangliang.too@foxmail.com>
2025-02-27 14:52:09 +0800

    [common] test

--- common/pkg/config/config.go
+++ common/pkg/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// Env interface 定义
+type Env interface {
+	Get(key string) (interface{}, error)
+	Set(key string, value interface{}) error
+	IsDevelopment() bool
+}
+
+// NewEnv 函数
+func NewEnv(env Env) Env {
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(&env)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	if env.IsDevelopment() {
+		log.Println("The App is running in development env")
+	}
+
+	return env
+}
